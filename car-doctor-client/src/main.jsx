e5c9c8e3fb2b150cf2882a010c5f67ca9bc17b80
MostakemHossain <mostakememon123@gmail.com>
2023-10-02 16:22:28 +0600

    create context api

--- car-doctor-client/src/main.jsx
+++ car-doctor-client/src/main.jsx
@@ -7,11 +7,14 @@ import {
 } from "react-router-dom";
 import "./index.css";
 import router from './routes/routes.jsx';
+import AuthProvider from './providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='max-w-7xl mx-auto'>
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </React.StrictMode>,
   </div>
 )
